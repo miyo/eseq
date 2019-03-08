11f8bdd22e1dff73558a6f04cc57adeb870159f8
Takefumi MIYOSHI <miyo@wasamon.net>
2019-03-09 00:03:08 +0900

    add cnot in qbit

--- sources/qbit.vhd
+++ sources/qbit.vhd
@@ -17,6 +17,11 @@ entity qbit is
     operator    : in std_logic_vector(7 downto 0);
     operator_en : in std_logic;
 
+    info_cnot_out   : out std_logic_vector(7 downto 0);
+    info_cnot_valid : out std_logic;
+    info_cnot_en    : in  std_logic;
+    info_cnot_in    : in  std_logic_vector(7 downto 0);
+
     phi   : out std_logic_vector(31 downto 0);
     theta : out std_logic_vector(31 downto 0)
     );
@@ -69,29 +74,49 @@ begin
   begin
     if rising_edge(clk) then
       if reset = '1' then
-        theta_reg <= to_signed(0, theta_reg'length);
-        phi_reg <= to_signed(0, phi_reg'length);
+        theta_reg       <= to_signed(0, theta_reg'length);
+        phi_reg         <= to_signed(0, phi_reg'length);
+        info_cnot_valid <= '0';
+      elsif info_cnot_en = '1' then
+        if info_cnot_in = X"01" then
+          -- X
+          theta_reg <= 180 - theta_reg; 
+          phi_reg <= 360 - shift_left(phi_reg, 1);
+        elsif info_cnot_in = X"03" then
+          -- entanglement
+          theta_reg <= 90;
+          phi_reg <= 0;
+        end if;
       elsif operator_en = '0' and op_reg = OP_FREERUN and valid = '1' then
-          theta_reg <= signed(theta_i);
-          phi_reg <= signed(phi_i);
+        theta_reg       <= signed(theta_i);
+        phi_reg         <= signed(phi_i);
+        info_cnot_valid <= '0';
       elsif operator_en = '1' then
-        op_reg <= operator;
+        op_reg          <= operator;
+        info_cnot_valid <= '0';
         
         case operator is
           when OP_FREERUN =>
             if valid = '1' then
               theta_reg <= signed(theta_i);
-              phi_reg <= signed(phi_i);
+              phi_reg   <= signed(phi_i);
             end if;
           when OP_MEASURE =>
             null;
           when OP_CNOT =>
-            null;
+            if theta_reg = 90 and phi_reg = 0 then
+              info_cnot <= X"03";
+            elsif theta_reg = 180 then
+              info_cnot_out <= X"01";
+            else
+              info_cnot_out <= X"00";
+            end if;
+            info_cnot_valid <= '1';
           when OP_X =>
             theta_reg <= 180 - theta_reg;
-            phi_reg <= 360 - shift_left(phi_reg, 1);
+            phi_reg   <= 360 - shift_left(phi_reg, 1);
           when OP_Y =>
-            theta_reg <= 180 - theta_reg;
+            theta_reg  <= 180 - theta_reg;
             if phi_reg <= 180 then
               phi_reg <= 180 - phi_reg;
             elsif phi_reg > 180 then
@@ -108,46 +133,46 @@ begin
           when OP_H =>
             if ((phi_reg <= 45) or (phi_reg >= 315)) then
               if (theta_reg <= 45) then
-                phi_reg <= to_signed(0, phi_reg'length);
+                phi_reg   <= to_signed(0, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               elsif (theta_reg <= 135) then
-                phi_reg <= to_signed(0, phi_reg'length);
+                phi_reg   <= to_signed(0, phi_reg'length);
                 theta_reg <= to_signed(0, theta_reg'length);
               elsif (theta_reg <= 180) then
-                phi_reg <= to_signed(180, phi_reg'length);
+                phi_reg   <= to_signed(180, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               elsif ((phi_reg > 45) and (phi_reg <= 135)) then
                 if (theta_reg <= 45) then
-                  phi_reg <= to_signed(0, phi_reg'length);
+                  phi_reg   <= to_signed(0, phi_reg'length);
                   theta_reg <= to_signed(90, theta_reg'length);
                 elsif (theta_reg <= 135) then
-                  phi_reg <= to_signed(270, phi_reg'length);
+                  phi_reg   <= to_signed(270, phi_reg'length);
                   theta_reg <= to_signed(90, theta'length);
                 elsif (theta_reg <= 180) then
-                  phi_reg <= to_signed(180, phi_reg'length);
+                  phi_reg   <= to_signed(180, phi_reg'length);
                   theta_reg <= to_signed(90, theta_reg'length);
                 end if;
               end if;
             elsif ((phi_reg > 135) and (phi_reg <= 225)) then
               if (theta_reg <= 45) then
-                phi_reg <= to_signed(0, phi_reg'length);
+                phi_reg   <= to_signed(0, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               elsif (theta_reg <= 135) then
-                phi_reg <= to_signed(0, phi_reg'length);
+                phi_reg   <= to_signed(0, phi_reg'length);
                 theta_reg <= to_signed(180, theta_reg'length);
               elsif (theta_reg <= 180) then
-                phi_reg <= to_signed(180, phi_reg'length);
+                phi_reg   <= to_signed(180, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               end if;
-            elsif ((phi_reg > 225) and phi_reg <315) then
+            elsif ((phi_reg > 225) and phi_reg < 315) then
               if (theta_reg <= 45) then
-                phi_reg <= to_signed(0, phi_reg'length);
+                phi_reg   <= to_signed(0, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               elsif (theta_reg <= 135) then
-                phi_reg <= to_signed(90, phi_reg'length);
+                phi_reg   <= to_signed(90, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               elsif (theta_reg <= 180) then
-                phi_reg <= to_signed(180, phi_reg'length);
+                phi_reg   <= to_signed(180, phi_reg'length);
                 theta_reg <= to_signed(90, theta_reg'length);
               end if;
             end if;
